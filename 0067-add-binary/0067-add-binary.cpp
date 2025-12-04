7cb2ce4f1ceb97eae660a4c6e8bc0725477bb54e
jagadeeshchinta <140415173+jagadeeshchinta@users.noreply.github.com>
2025-12-04 20:28:15 +0530

    Time: 4 ms (10.5%), Space: 12.3 MB (6.22%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     string addBinary(string al, string bl) {
-        int a=al.size();
-        int b=bl.size();
+        int a=al.size()-1;
+        int b=bl.size()-1;
         int carry=0;
         string ans="";
         int i=0;
-        while(i<a || i<b || carry!=0){
+        while(i<=a || i<=b || carry!=0){
             int x=0;
-            if(i<a && al[a-i-1]=='1'){
+            if(i<=a && al[a-i]=='1'){
                 x=1;
             }
             int y=0;
-            if(i<b && bl[b-i-1]=='1'){
+            if(i<=b && bl[b-i]=='1'){
               y=1;
             }
             ans=to_string((x+y+carry)%2)+ans;
