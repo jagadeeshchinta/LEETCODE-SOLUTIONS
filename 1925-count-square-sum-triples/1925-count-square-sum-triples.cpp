ccc3f060e38736c00226d42a4f89e33e4497fcdd
jagadeeshchinta <140415173+jagadeeshchinta@users.noreply.github.com>
2025-12-08 22:07:10 +0530

    Time: 4 ms (77.21%), Space: 7.9 MB (21.68%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -4,10 +4,10 @@ public:
         int count=0;
         for(int a=1;a<=n;a++){
             for(int b=1;b<=n;b++){
-                for(int c=1;c<=n;c++){
-                    if((a*a + b*b) == c*c){
-                        count++;
-                    }
+                int c2=a*a+b*b;
+                int c=sqrt(c2);
+                if(c*c==c2 && c<=n){
+                    count++;
                 }
             }
         }
