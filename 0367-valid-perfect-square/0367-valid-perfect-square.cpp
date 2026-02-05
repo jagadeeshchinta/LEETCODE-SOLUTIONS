270b1d6f30b07cada20e9b1ac04a4c4e8f08b743
jagadeeshchinta <140415173+jagadeeshchinta@users.noreply.github.com>
2026-02-05 10:03:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+      for(int i=0;i*i<=num;i++){
+        if(i*i==num){
+            return true;
+        }
+      } 
+      return false;
+    }
+};
